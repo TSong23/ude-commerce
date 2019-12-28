24646858e62c8946f38b53f64a7a8076db4c3dc3
Taehoon <songyi3279@gmail.com>
2019-12-27 23:57:04 -0800

    lesson 2 done. basic homepage setup with some css

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './homepage.styles.scss';
+
 const HomePage = () => (
   <div className='homepage'>
     <div className='directory-menu'>
@@ -13,28 +15,7 @@ const HomePage = () => (
 
       <div className='menu-item'>
         <div className='content'>
-          <h1 className='title'>WOMENS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>WOMENS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>WOMENS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>WOMENS</h1>
+          <h1 className='title'>MENS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
@@ -42,3 +23,5 @@ const HomePage = () => (
     </div>
   </div>
 )
+
+export default HomePage;
