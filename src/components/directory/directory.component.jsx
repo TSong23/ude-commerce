aa1c5de3103501939e85a0f04b19ea638ea1e31d
Taehoon <songyi3279@gmail.com>
2019-12-29 13:58:58 -0800

    lesson 62 complete

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+
+
+import './directory.component.scss';
+
+
+
+class Directory extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      sections: [
+        {
+          titl
+        }
+      ]
+
+    }
+  }
+
+  render(){
+
+    return(
+      <div className='directory-menu'>
+        {
+
+        }
+      </div>
+    )
+  }
+}
