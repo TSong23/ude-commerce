4ca4ef38710cf98fb07d598e19442232c4ba2217
Taehoon <songyi3279@gmail.com>
2019-12-30 15:13:06 -0800

    lesson 70 done. created preview component. routing issue: homepage not showing up with <Switch>. currently using <Route excat>.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import {Switc, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 // homepage url can be '/' using react route
 
 function App() {
   return (
     <div >
-      <Switch>
+  
         <Route exact path='/' component={HomePage} />
-      </Switch>
+        <Route exact path='/shop' component={ShopPage} />
+
+    
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import ShopData from './shop.data';
+import Preview from '../../components/menu-item/preview/preview.component';
+
+class ShopPage extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      collections: ShopData,
+    }
+
+  }
+
+  render() {
+
+    const {collections} = this.state;
+
+    return(
+
+      <div className='shop-page'>
+        {
+          collections.map( ({id, ...otherPreviewProps}) => (
+            <Preview key={id} {...otherPreviewProps} ></Preview>
+          ))  
+        }
+      </div>
+    )
+  }
+
+}
+
+export default ShopPage;
