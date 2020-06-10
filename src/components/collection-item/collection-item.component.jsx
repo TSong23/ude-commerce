4718408a72dd4bf4d941b061b3136a5466721581
Taehoon <songyi3279@gmail.com>
2020-06-10 15:51:10 -0700

    reorganizing the routing for the site. Shop component will list categories of products and home page will just show random nuieun pictures

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import './collection-item.style.scss';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
 
 const CollectionItem = ({id, imageUrl,name, price }) => (
@@ -18,6 +18,7 @@ const CollectionItem = ({id, imageUrl,name, price }) => (
       <span className='price'>{price}</span>
     </div> */}
 
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 
 )
