4aa6c1e6323140705327b7ea38f02ccb99ee27ca
Taehoon <songyi3279@gmail.com>
2020-06-17 19:08:52 -0700

    fixed routing for shop page. when each category is clicked, it will reload a view page for that category

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends React.Component {
         <Switch>
 
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/shop' component={ShopPage} />
           <Route exact path='/signin' render={() => 
             this.props.currentUser ? (<Redirect to='/' />) : <SignInAndSignUpPage/>
           } />
