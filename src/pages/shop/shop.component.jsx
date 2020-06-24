41ab4a0715ac7d354c207007bbb0de590484badb
Taehoon <songyi3279@gmail.com>
2020-06-24 02:22:07 -0700

    nuieun shop page design complete

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,9 +5,6 @@ import Preview from '../../components/preview/preview.component';
 import MenuItem from '../../components/menu-item/menu-item.component';
 import './shop.styles.scss';
 
-import PorcelainCategory from '../../assets/porcelain_category_image.jpg';
-import OthersCategory from '../../assets/others_category_image.jpg';
-
 // Need to code out what happens when you click on MenuItem
 // currently, it's blank. Probably should load a page(React Component)
 // that has repective items such as porcelain and etc. 
@@ -21,43 +18,43 @@ class ShopPage extends React.Component{
       // collections: ShopData,
       sections: [
         {
-          title: 'Porcelain',
-          imageUrl: PorcelainCategory,
+          title: 'Kitchen',
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ude-commerse.appspot.com/o/Kitchen%2FIMG_3550.jpeg?alt=media&token=b4d5bc0d-0af7-4621-9d30-ae2e88e0a2fd',
           size: 'large',
           id: 1,
-          linkUrl: '/porcelain'
+          linkUrl: '/kitchen'
         },
         {
           title: 'Stationary',
-          imageUrl: OthersCategory,
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ude-commerse.appspot.com/o/Stationary%2FIMG_0866.jpeg?alt=media&token=de5e976f-7478-40b1-91ac-aca409380324',
           size: 'large',
           id: 2,
           linkUrl: '/stationary'
         },
         {
           title: 'Fabric',
-          imageUrl: OthersCategory,
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ude-commerse.appspot.com/o/Fabric%2FIMG_0200.jpeg?alt=media&token=50fb5046-c541-42c7-9338-be81a7676d00',
           size: 'large',
           id: 3,
           linkUrl: '/fabric'
         },
         {
           title: 'Home',
-          imageUrl: OthersCategory,
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ude-commerse.appspot.com/o/Other%2FIMG_2286.jpg?alt=media&token=5df4c96d-c795-428f-87fc-6a206c9dc136',
           size: 'large',
           id: 4,
           linkUrl: '/home'
         },
         {
           title: 'Accessory',
-          imageUrl: OthersCategory,
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ude-commerse.appspot.com/o/Accessory%2FIMG_0200.jpeg?alt=media&token=751a5aa1-343e-4bb6-9f14-bc1024a143c5',
           size: 'large',
           id: 5,
           linkUrl: '/accessory'
         },
         {
           title: 'About',
-          imageUrl: OthersCategory,
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ude-commerse.appspot.com/o/Other%2FIMGL0566.jpg?alt=media&token=b6870fc4-3cc3-45ea-be0b-e7b26fb3d4be',
           size: 'large',
           id: 6,
           linkUrl: '/about'
