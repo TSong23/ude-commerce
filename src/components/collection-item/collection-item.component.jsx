42de106cedc366653a59991475dcde288ec10693
Taehoon <songyi3279@gmail.com>
2020-06-28 18:26:00 -0700

    more of css done

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -14,7 +14,7 @@ const CollectionItem = ({id, imageUrl,name, price }) => (
 
     <div className="collection-footer">
       <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+    <span className='price'>${`${price}`}</span>
     </div>
 
     <CustomButton inverted>Add to cart</CustomButton>
